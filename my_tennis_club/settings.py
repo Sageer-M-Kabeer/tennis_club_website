d2ddd01590059d4904ea0cd71d767d6e3c0d24b8
Sageer-M-Kabeer <Sageerm379@gmail.com>
2023-07-23 10:50:33 +0000

    secrect key

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -10,7 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!t#1pr%*!y(pyltmrj7-s(j@h9piyoh=*k8(u#xh!pmk@jg#!^'
+from django.core.management.utils import get_random_secret_key
+
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True 
